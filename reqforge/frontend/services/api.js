fa911932988cfc3af1d716ad2ec7ccb068f0d29a
Navaneet b <navaneetmb17@gmail.com>
2026-01-30 12:00:33 +0530

    Update API base URL to production

--- reqforge/frontend/services/api.js
+++ reqforge/frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://reqforge.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
